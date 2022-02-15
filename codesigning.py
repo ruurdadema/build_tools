535790c87041678b1f69ce1977af83e70af9071e
Ruurd Adema <ruurd@owllab.nl>
2022-02-15 09:25:23 +0100

    Add sign file function

--- codesigning.py
+++ codesigning.py
@@ -4,13 +4,46 @@ import subprocess
 import re
 
 
-def find_codesigning_developer_id_application_identities():
+def sign_file(file, codesign_identity=None, development_team=None, organization=None):
+    if codesign_identity is None:
+        result = get_codesigning_developer_id_application_identities(team=development_team,
+                                                                     organization=organization)[0]
+        if result:
+            print('Found codesign identity: {}'.format(result))
+
+        codesign_identity = result[0]
+
+    if codesign_identity is None:
+        raise 'Failed to find a code signing identity'
+
+    print('Signing file \"{}\" with identity \"{}\"'.format(file, codesign_identity))
+
+    subprocess.call(['codesign',
+                     '--force',
+                     '--timestamp',
+                     '--options=runtime',
+                     '-s', codesign_identity,
+                     file])
+
+    subprocess.call(['codesign', '-vv', '-d', file])
+
+
+def get_codesigning_developer_id_application_identities(organization=None, team=None):
     lines = subprocess.check_output(['security', 'find-identity', '-p', 'codesigning', '-v']).splitlines()
     ids = []
 
     for line in lines:
         match = re.search(r'.+\d+\) (\w+) "Developer ID Application: (.+) \((.+)\)"', line.decode())
+
         if match:
+            # If an organization was specified, we only add this identity if it matches this organization.
+            if organization and organization != match.groups()[1]:
+                continue
+
+            # If team was specified, we only add this identity if it matches this team.
+            if team and team != match.groups()[2]:
+                continue
+
             ids.append(match.groups())
 
     return ids
