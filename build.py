b261c809a610d42fbce4193bdc6d65721ef64db1
Ruurd Adema <ruurd@owllab.nl>
2025-04-25 13:01:30 +0200

    Burn cmake version to not depend on git (RAV-102)

--- build.py
+++ build.py
@@ -226,6 +226,7 @@ def build_dist(args):
 
     # Manually choose the files to copy to prevent accidental leaking of files when the repo changes or is not clean.
 
+    copytree('cmake', path_to_dist / 'cmake', dirs_exist_ok=True)
     copytree('source', path_to_dist / 'source', dirs_exist_ok=True)
     copy2('.clang-format', path_to_dist)
     copy2('.gitignore', path_to_dist)
@@ -249,6 +250,20 @@ def build_dist(args):
 
     with open(path_to_dist / 'version.json', 'w') as file:
         json.dump(version_data, file, indent=4)
+
+    match = re.match(r"^v([0-9]+)\.([0-9]+)\.([0-9]+)(.*)$", git_version)
+
+    if not match:
+        raise ValueError(f'Invalid version {git_version}')
+
+    with open(path_to_dist / 'cmake/version.cmake', 'w') as file:
+        file.write(f'# Auto generated (see build.py)\n\n')
+        file.write(f'# {app_name}\n\n')
+        file.write(f'set(GIT_DESCRIBE_VERSION "{git_version}")\n')
+        file.write(f'set(GIT_VERSION_MAJOR {match.group(1)})\n')
+        file.write(f'set(GIT_VERSION_MINOR {match.group(2)})\n')
+        file.write(f'set(GIT_VERSION_PATCH {match.group(3)})\n')
+        file.write(f'set(BUILD_NUMBER {args.build_number})\n')
 
     # Create ZIP from archive
     archive_path = args.path_to_build + '/ravennakit-juce-demo-' + git_version + '-' + args.build_number + '-dist'
