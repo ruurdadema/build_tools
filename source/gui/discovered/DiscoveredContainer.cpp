d2b7f2c35118b9fb31fb13b7f66765fb94fd5124
Ruurd Adema <ruurd@owllab.nl>
2025-11-26 23:03:06 +0100

    Update license (RAV-151)

--- source/gui/discovered/DiscoveredContainer.cpp
+++ source/gui/discovered/DiscoveredContainer.cpp
@@ -1,11 +1,22 @@
 /*
- * Owllab License Agreement
+ * Project: RAVENNAKIT demo application
+ * Copyright (c) 2024-2025 Sound on Digital
  *
- * This software is provided by Owllab and may not be used, copied, modified,
- * merged, published, distributed, sublicensed, or sold without a valid and
- * explicit agreement with Owllab.
+ * This file is part of the RAVENNAKIT demo application.
  *
- * Copyright (c) 2025 Owllab. All rights reserved.
+ * The RAVENNAKIT demo application is licensed on a proprietary,
+ * source-available basis. You are allowed to view, modify, and compile
+ * this code for personal or internal evaluation only.
+ *
+ * You may NOT redistribute this file, any modified versions, or any
+ * compiled binaries to third parties, and you may NOT use it for any
+ * commercial purpose, except under a separate written agreement with
+ * Sound on Digital.
+ *
+ * For the full license terms, see:
+ *   LICENSE.md
+ * or visit:
+ *   https://ravennakit.com
  */
 
 #include "DiscoveredContainer.hpp"
